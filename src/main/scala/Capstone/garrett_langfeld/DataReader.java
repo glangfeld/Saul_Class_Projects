a11d74e2762ecb999acca390f2309ba5e71ae86c
glangfeld <garrett.langfeld@thredup.com>
2016-09-19 23:17:30 -0500

    Create DataReader.java

---
+++ src/main/scala/Capstone/garrett_langfeld/DataReader.java
@@ -0,0 +1,30 @@
+/**
+ * Created by glang on 9/19/2016.
+ */
+package com.howtodoinjava.csv.demoScanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DataReader
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        //Get scanner instance
+        Scanner scanner = new Scanner(new File("C:\\Users\\glang\\OneDrive\\Documents\\Tulane\\Senior Year First Semester\\Capstone\\Initial_Data.csv"));
+
+        //Set the delimiter used in file
+        scanner.useDelimiter(",");
+
+        //Get all tokens and store them in some data structure
+        //I am just printing them
+        while (scanner.hasNext())
+        {
+            System.out.print(scanner.next() + "|");
+        }
+
+        //Do not forget to close the scanner
+        scanner.close();
+    }
+}
