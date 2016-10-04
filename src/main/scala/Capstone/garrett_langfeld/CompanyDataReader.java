85dfa766727b8ac8347de05dee941aa3db5d1dfa
Garrett Langfeld <Garrett Langfeld>
2016-10-03 22:53:42 -0500

    read in all of the data

--- src/main/scala/Capstone/garrett_langfeld/CompanyDataReader.java
+++ src/main/scala/Capstone/garrett_langfeld/CompanyDataReader.java
@@ -10,14 +10,18 @@ public class CompanyDataReader
 
     //public String[] allCompanies;
     public ArrayList allCompanies;
+    public ArrayList<ArrayList<ArrayList>> allData;
 
     //constructor
     public CompanyDataReader() throws IOException{
         //initializing allCompanies arrayList
         allCompanies = new ArrayList();
+        allData = new ArrayList<ArrayList<ArrayList>>();
 
         BufferedReader br = new BufferedReader(new FileReader("/Users/glang/OneDrive/Documents/Tulane/Senior Year First Semester/Capstone/Saul_Class_Projects/data/garrett_langfeld/Initial_Data.csv"));
         String line="";
+        //used to keep track of place in outer allData arraylist
+        int comp_index = -1;
         while ((line = br.readLine()) != null) {
             //skipping first row with headings
             if (line.substring(0,7).equals("Company")){
@@ -32,9 +36,28 @@ public class CompanyDataReader
                 if (line.charAt(0) != ',') {
                     //getting company name
                     String[] companyName = line.split(csvSplitBy);
-                    System.out.println(companyName[0]);
+                    //System.out.println(companyName[0]);
                     //System.out.println(line.split(csvSplitBy)[0]);
                     allCompanies.add(companyName[0]);
+                    //incrementing comp_index
+                    comp_index++;
+                    //creating middle arrayList, then adding to allData
+                    ArrayList<ArrayList> middle = new ArrayList<ArrayList>();
+                    allData.add(middle);
+                }
+                //adding data to allData arraylist of arraylists of arraylists
+                else{
+                    ArrayList inner = new ArrayList();
+                    String[] row = line.split(csvSplitBy);
+                    if (row.length > 0){
+                        for (int i = 0; i < row.length; i++){
+                            if(row[i].length() > 0) {
+                                inner.add(row[i]);
+                            }
+                        }
+                    }
+                    //System.out.println(inner);
+                    allData.get(comp_index).add(inner);
                 }
                 String[] cols = line.split(csvSplitBy);
                 //System.out.println("Column 0 = " + cols[0] + " , Column 4= " + cols[4] + " , Column 5=" + cols[5]);
@@ -46,6 +69,7 @@ public class CompanyDataReader
         }
 
         System.out.println(allCompanies);
+        System.out.println(allData);
 
         /*
         //Get scanner instance
