eea125b4ba0f9646285dd917892eb21f7999def2
Garrett Langfeld <glang@DESKTOP-KEG8JDA.(none)>
2017-03-31 23:17:52 -0700

    got saul working with compustat data

---
+++ src/main/scala/Capstone/garrett_langfeld/CompanyClassifier2.scala
@@ -0,0 +1,152 @@
+package Capstone.garrett_langfeld
+
+/**
+  * Created by glang on 3/31/2017.
+  */
+
+import edu.illinois.cs.cogcomp.lbjava.learn.SupportVectorMachine
+import edu.illinois.cs.cogcomp.lbjava.learn.SparseNetworkLearner
+import edu.illinois.cs.cogcomp.saul.classifier.Learnable
+import edu.illinois.cs.cogcomp.lbjava.learn.StochasticGradientDescent
+import weka.classifiers.bayes.NaiveBayes
+import weka.classifiers.bayes.BayesNet
+import weka.classifiers.trees.RandomForest
+import weka.classifiers.functions.MultilayerPerceptron
+import edu.illinois.cs.cogcomp.saul.learn.SaulWekaWrapper
+import edu.illinois.cs.cogcomp.saul
+import Readers.garrett_langfeld.companyDataCompustat
+import weka.classifiers.meta.AdaBoostM1
+
+object CompanyClassifier2 {
+  import CompanyDataModel2._
+
+  /*
+  object firstCompanyClassifier extends Learnable[companyDataCompustat](comp) {
+    def label = risk
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, net_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    override def feature = using(netDebtEBITDA, total_debt_ebit, tot_DA, LTD_eq)
+    override lazy val classifier = new SupportVectorMachine()
+
+  }
+
+  object secondCompanyClassifier extends Learnable[companyDataCompustat](comp) {
+    //def label = rating
+    //def label = ratingLetter
+    def label = risk
+    override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, tot_DA, LTD_eq)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    override lazy val classifier = new SupportVectorMachine()
+
+  }
+
+  object CompanyClassifierSparseNetwork extends Learnable[companyDataCompustat](comp) {
+    def label = risk
+    override def feature = using(netDebtEBITDA, total_debt_ebit, net_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, tot_DA, LTD_eq)
+    override lazy val classifier = new SparseNetworkLearner()
+
+  }
+
+  object CompanyClassifierWeka extends Learnable(comp) {
+    def label = rating
+    override lazy val classifier = new SaulWekaWrapper(new NaiveBayes())
+    override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+  }
+
+  object CompanyClassifierBayesNetwork extends Learnable(comp) {
+    //def label = rating
+    //def label = ratingLetter
+    def label = risk
+    override lazy val classifier = new SaulWekaWrapper(new BayesNet())
+    override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+  }
+
+  object CompanyClassifierRandomForest extends Learnable(comp) {
+    //def label = rating
+    //def label = ratingGroup
+    def label = ratingLetter
+    //def label = risk
+    //def label = rating6Groups
+    override lazy val classifier = new SaulWekaWrapper(new RandomForest())
+    //override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    override def feature = using(netDebtEBITDA, total_debt_ebit, net_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(tot_DE, int_exp, ebit_int_exp, ebitda_capex_int, eq_tot_assets, tot_DA)
+  }
+
+  object CompanyClassifierRandomForest2 extends Learnable(comp) {
+    //def label = rating
+    //def label = ratingGroup
+    //def label = ratingLetter
+    def label = rating6Groups
+    override lazy val classifier = new SaulWekaWrapper(new RandomForest())
+    //override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, net_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    override def feature = using(tot_DE, int_exp, ebit_int_exp, ebitda_capex_int, eq_tot_assets, tot_DA)
+  }
+
+  object CompanyClassifierRandomForest3 extends Learnable(comp) {
+    def label = risk
+    override lazy val classifier = new SaulWekaWrapper(new RandomForest())
+    //override def feature = using(total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //def label = rating
+    //def label = ratingGroup
+    //def label = ratingLetter
+    //override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    override def feature = using(netDebtEBITDA, total_debt_ebit, net_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA, sector)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(netDebtEBITDA, total_debt_ebit, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    //override def feature = using(tot_DE, int_exp, ebit_int_exp, ebitda_capex_int, eq_tot_assets, tot_DA)
+    //override def feature = using(total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+
+    //override def feature = using(total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, LTD_cap, tot_DE, tot_DC, tot_DA)
+  }
+
+
+
+  object CompanyClassifierMLPerceptron extends Learnable(comp) {
+    //def label = rating
+    def label = ratingLetter
+    override lazy val classifier = new SaulWekaWrapper(new MultilayerPerceptron())
+    //override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    override def feature = using(netDebtEBITDA, total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+  }
+
+  */
+
+  object CompanyClassifierAdaBoost extends Learnable(comp) {
+    //def label = rating
+    def label = rating
+    override lazy val classifier = new SaulWekaWrapper(new AdaBoostM1())
+    //override def feature = using(eq_tot_assets, LTD_eq, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+    override def feature = using(company, LTD_IC, TD_IC, R_CA, TD_TA, TD_EBITDA, LTD_TL, CF_TD, LTD_BE, sector, TD_A, TD_C, TD_E, Int_Cov, quick_ratio, current_ratio, cash_conv)
+    //override def feature = using(total_debt_ebit, ebitda_int_exp, ebitda_capex_int, ebit_int_exp, int_exp, LTD_cap, LTD_tot_assets, tot_DE, tot_DC, tot_DA)
+  }
+
+
+
+  /*
+    object CompanyClassifierWeka extends Learnable(comp) {
+      def label = rating
+      //override lazy val classifier = new SaulWekaWrapper(new NaiveBayes())
+      //override def feature = using(wordFeature)
+    }
+  */
+
+}
+
---
+++ src/main/scala/Readers/garrett_langfeld/CompanyDataReaderCompustat.java
@@ -0,0 +1,96 @@
+package Readers.garrett_langfeld;
+
+/**
+ * Created by glang on 3/31/2017.
+ */
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class CompanyDataReaderCompustat {
+    //public String[] allCompanies;
+    public ArrayList allCompanies;
+    public ArrayList<ArrayList<ArrayList<String>>> allData;
+    public ArrayList<companyDataCompustat> compData;
+
+    //constructor
+    public CompanyDataReaderCompustat() throws IOException{
+        //initializing allCompanies arrayList
+        allCompanies = new ArrayList();
+        allData = new ArrayList<ArrayList<ArrayList<String>>>();
+        compData = new ArrayList<companyDataCompustat>();
+
+
+        //BufferedReader br = new BufferedReader(new FileReader("/Users/glang/OneDrive/Documents/Tulane/Senior Year First Semester/Capstone/Saul_Class_Projects/data/garrett_langfeld/Initial_Data11.csv"));
+        BufferedReader br = new BufferedReader(new FileReader("/Users/glang/OneDrive/Documents/Tulane/Senior Year First Semester/Capstone/Saul_Class_Projects/data/garrett_langfeld/Compustat/Compustat_Data_NN_2.csv"));
+        String line="";
+        //used to keep track of place in outer allData arraylist
+        int comp_index = -1;
+        while ((line = br.readLine()) != null) {
+            //skipping first row with headings
+            if (line.substring(0,4).equals("Data") || line.substring(0,7).equals(",,,,,,,")){
+                //skip this line
+            }
+            else {
+                //System.out.println(line.charAt(0));
+                //System.out.println(line);
+                //System.out.println(line.substring(0, 7));
+                // use comma as separator
+                String csvSplitBy = ",";
+
+                ArrayList inner = new ArrayList<companyDataCompustat>();
+                String[] row = line.split(csvSplitBy);
+                if (row.length > 0){
+                    float date = Float.parseFloat(row[0]);
+                    float company = Float.parseFloat(row[1]);
+                    float LTD_IC = Float.parseFloat(row[2]);
+                    float TD_IC = Float.parseFloat(row[3]);
+                    float R_CA = Float.parseFloat(row[4]);
+                    float TD_TA = Float.parseFloat(row[5]);
+                    float TD_EBITDA = Float.parseFloat(row[6]);
+                    float LTD_TL = Float.parseFloat(row[7]);
+                    float CF_TD = Float.parseFloat(row[8]);
+                    float LTD_BE = Float.parseFloat(row[9]);
+                    float TD_A = Float.parseFloat(row[10]);
+                    float TD_C = Float.parseFloat(row[11]);
+                    float TD_E = Float.parseFloat(row[12]);
+                    float Int_Cov = Float.parseFloat(row[13]);
+                    float quick_ratio = Float.parseFloat(row[14]);
+                    float current_ratio = Float.parseFloat(row[15]);
+                    float cash_conv = Float.parseFloat(row[16]);
+                    //float rating = Float.parseFloat(row[17]);
+                    String rating = row[17];
+                    float sector = Float.parseFloat(row[18]);
+
+                    companyDataCompustat comp = new companyDataCompustat(date,company,LTD_IC,TD_IC,R_CA,TD_TA,TD_EBITDA,LTD_TL,CF_TD,LTD_BE,TD_A,TD_C,TD_E,Int_Cov,quick_ratio,current_ratio,cash_conv,rating,sector);
+                    compData.add(comp);
+                }
+
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) throws IOException
+    {
+
+        CompanyDataReaderCompustat c = new CompanyDataReaderCompustat();
+        c.compData.get(5).printCompany();
+        int i =1;
+
+
+    }
+
+
+}
